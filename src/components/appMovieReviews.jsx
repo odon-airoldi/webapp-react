c409ff830993432aee1b5f18d410c3985b9ae4c9
odon-airoldi <odon.airoldi@gmail.com>
2026-04-22 22:51:48 +0200

    add review vote with stars

--- src/components/appMovieReviews.jsx
+++ src/components/appMovieReviews.jsx
@@ -1,12 +1,27 @@
 export default function AppMovieReviews({ review }) {
 
+    let i = 0;
+    let stars = []
+
+    while (i < review.vote) {
+        stars.push(i)
+        i++;
+    }
 
     return (
         <div className="py-3">
             <div>{review.name} Vote: {review.vote}</div>
+            <div>
+                {
+                    stars.map((star, i) => (
+                        <span key={i}>*</span>
+                    ))
+                }
+            </div>
             <div>{review.text}</div>
             <div>{new Date(review.created_at).toLocaleDateString("it-IT")}</div>
         </div>
     )
+
 }
 
