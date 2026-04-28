7ebb34a8d241174cb295452a4e348b69e0ffe252
odon-airoldi <odon.airoldi@gmail.com>
2026-04-28 18:16:34 +0200

    add style movie card and modal

--- src/components/AppCard.jsx
+++ src/components/AppCard.jsx
@@ -4,12 +4,12 @@ export default function AppCard({ movie, openModal }) {
 
     return (
         <div>
-            <div className="card text-decoration-none" onClick={openModal}>
+            <div className="movie-card shadow cursor-pointer" onClick={openModal}>
                 <div className="">
-                    <img src={`http://localhost:3010/img/${movie.image}`} className="card-img-top ratio ratio-3x4 object-fit-cover" alt="..." />
+                    <img src={`http://localhost:3010/img/${movie.image}`} className="ratio ratio-3x4 object-fit-cover" alt={movie.title} />
                 </div>
-                <div className="card-body">
-                    <h5 className="card-title mb-0">{movie.title}</h5>
+                <div className="p-3">
+                    <h5 className="mb-0">{movie.title}</h5>
                 </div>
             </div>
         </div >
--- src/components/AppMovieModal.jsx
+++ src/components/AppMovieModal.jsx
@@ -10,12 +10,16 @@ export default function AppMovieModal() {
     return (
         <>
             {modalMovie && (
-                <div className="bg-primary bg-opacity-25 position-fixed top-0 bottom-0 start-0 end-0 d-flex align-items-center justify-content-center">
-                    <div className="modal-card bg-white p-3">
+                <div className="backdrop position-fixed top-0 bottom-0 start-0 end-0 d-flex align-items-center justify-content-center">
+                    <div className="modal-card bg-white p-3 position-relative">
                         <h3 className="h5">{modalMovie.title}</h3>
                         <p>{modalMovie.abstract}</p>
-                        <Link className="" to={`movie/${modalMovie.id}`} onClick={() => setModalMovie(null)}>View</Link>
-                        <div onClick={() => setModalMovie(null)}>close</div>
+                        <Link className="btn btn-secondary" to={`movie/${modalMovie.id}`} onClick={() => setModalMovie(null)}>View</Link>
+                        <div className="cursor-pointer position-absolute top-0 start-100 translate-middle bg-black p-1 rounded-circle" onClick={() => setModalMovie(null)}>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="rgb(255,255,255)" className="d-block" viewBox="0 0 16 16">
+                                <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z" />
+                            </svg>
+                        </div>
                     </div>
                 </div>
             )}
